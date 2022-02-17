f412c1323f6119f25b6a5e991de16ebb909da32c
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-02-17 16:59:34 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,9 +1,17 @@
+#!/usr/bin/python3
+# Test case for adding two numbers
 import unittest
+
 from Prog1 import summation
+
 class TestSum(unittest.TestCase):
-  def test_list_int(self):
-    data=[22,33]
-    result=summation(data)
-    self.assertEqual(result,55)
-if __name__=='__main__":
-  unitest.main()
+    def test_list_int(self):
+        """
+        Test case to add two numbers
+        """
+        data = [2, 3]
+        result = summation(data)
+        self.assertEqual(result, 5)
+
+if __name__ == '__main__':
+    unittest.main()
